b2527ac76dbcc8150bc539d460cd8387dd37e5b9
Salman Khan <kay2dan@gmail.com>
2021-07-02 11:08:23 +0500

    generations script is working well except for a bug where the last img is extracted instead of the first

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-const fs = require("fs");
+var fs = require("fs");
+const fse = require("fs-extra");
+const request = require("request");
+var https = require("https");
 const path = require("path");
-const axios = require("axios");
-const cheerio = require("cheerio");
+// const axios = require("axios");
+// const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 const createCSVWriter = require("csv-writer").createObjectCsvWriter;
 
@@ -15,6 +18,16 @@ const csvWriter = createCSVWriter({
     // { id: "Title", title: "Title" },
   ],
 });
+
+//Node.js Function to save image from External URL.
+function saveImageToDisk(url, name, localPath) {
+  request(url).pipe(fs.createWriteStream(`./data/img/${name}.png`));
+  // var fullUrl = url;
+  // var file = fs.createWriteStream(localPath);
+  // var request = https.get(url, function (response) {
+  //   response.pipe(file);
+  // });
+}
 
 (async () => {
   try {
@@ -48,7 +61,7 @@ const csvWriter = createCSVWriter({
     const collection = [];
 
     // loop starting from page 1 to last page
-    for (let i = 1; i <= 2; i++) {
+    for (let i = 1; i <= 3; i++) {
       // already on page 1, so no need to goto the page
       i > 1 ? await page.goto(`${site}${i}`) : false;
 
@@ -66,34 +79,40 @@ const csvWriter = createCSVWriter({
       collection.push(urls);
     }
 
+    const flatCollection = collection.flat();
+
     const payload = [];
     //  for(let urls of collection){
-    for (let j = 0; j < collection.length; j++) {
-      const pageCollection = collection[j];
-      // for (const o of urls) {
-      for (let k = 0; k < pageCollection.length; k++) {
-        const curRef = pageCollection[k];
-        const page = await browser.newPage();
+    for (let k = 0; k < flatCollection.length; k++) {
+      const curRef = flatCollection[k];
+      const page = await browser.newPage();
 
-        await page.goto(curRef.url);
-        let linkInfo = await page.evaluate(() => ({
-          img: document
-            .querySelector("div.all-img > a > img")
-            .getAttribute("src"),
-          title: document.querySelector("h1.page-title > span").innerHTML,
-          price: document.querySelector(
-            "div.product-info-price > span > span > span.price"
-          ).innerHTML,
-          // description: document.querySelector(
-          //   "div#product.info.overview > div.product > div.value > b"
-          // ).innerHTML,
-        }));
-        payload.push(linkInfo);
-        await page.close();
+      await page.goto(curRef.url);
+      let linkInfo = await page.evaluate(() => ({
+        img: document
+          .querySelector("div.all-img:first-child > a > img")
+          .getAttribute("src"),
+        title: document.querySelector("h1.page-title > span").innerHTML,
+        price: document.querySelector(
+          "div.product-info-price > span > span > span.price"
+        ).innerHTML,
+        // description: document.querySelector(
+        //   "div#product.info.overview > div.product > div.value > b"
+        // ).innerHTML,
+      }));
+      console.log("linkinfo: ", linkInfo);
+      try {
+        // const fPath = `./data/${linkInfo.title}`;
+        // await fs.promises.mkdir(fPath, {
+        //   recursive: true,
+        // });
+        saveImageToDisk(linkInfo.img, linkInfo.title);
+      } catch (err) {
+        console.log("err: ", err);
       }
+      payload.push(linkInfo);
+      await page.close();
     }
-
-    console.log("payload:", payload);
 
     await browser.close();
 
@@ -101,7 +120,7 @@ const csvWriter = createCSVWriter({
     // await fs.writeFileSync("./items.txt", payload.join("\n") + "\n");
 
     await csvWriter
-      .writeRecords(collection[0])
+      .writeRecords(flatCollection)
       .then(() => console.log("The CSV file was written successfully"));
   } catch (e) {
     console.log("e: ", e);
