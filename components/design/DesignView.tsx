95f340c3f22cbe1234e9b5c0726867ef1d7705cc
Salman Khan <kay2dan@gmail.com>
2021-07-24 15:45:46 +0500

    first draft of design page

--- components/design/DesignView.tsx
+++ components/design/DesignView.tsx
@@ -3,17 +3,27 @@ import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 const DesignView: React.FC<any> = ({ navigation }) => {
   return (
-    <View style={{ flex: 2 }}>
-      <View style={{ flex: 4, backgroundColor: "yellow" }}>
-        <Image resizeMethod={'auto'} resizeMode={"contain"} source={{ uri: "https://picsum.photos/seed/picsum/200/300" }} />
+    <View style={styles.mainView}>
+      {/* <Text style={{ textAlign: 'center' }}>Salam</Text> */}
+      <View style={styles.imgContainer}>
+        <Image resizeMethod={'auto'} resizeMode={"contain"} source={{ width: 600, height: 250, uri: "https://picsum.photos/seed/picsum/200/300" }} style={styles.img} />
       </View>
-      <View style={{ flex: 1 }}>
+      <View style={styles.btnsContainer}>
         <Button title="like" onPress={() => { }} />
         <Button title="dislike" onPress={() => { }} />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainView:
+    { width: "100vw", height: "100vh", backgroundColor: "yellow", borderColor: "black", borderWidth: 1, borderStyle: "solid" },
+  imgContainer:
+    { backgroundColor: "rgba(11,222, 123, 0.25)", width: "92%", height: "80%", marginLeft: "4%" },
+  img: { width: "100%", height: "100%" },
+  btnsContainer: { flex: 1, flexDirection: "row", justifyContent: "space-around", backgroundColor: "pink" }
+})
 
 export default DesignView;
 
