48d25bf48099d7a44c61c6aa2e8867b9417b1f39
Salman Khan <kay2dan@gmail.com>
2021-07-25 09:27:42 +0500

    added eslint and have the android app page rateDesign working

--- components/design/DesignView.tsx
+++ components/design/DesignView.tsx
@@ -4,7 +4,7 @@ import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 const DesignView: React.FC<any> = ({ navigation }) => {
   return (
     <View style={styles.mainView}>
-      {/* <Text style={{ textAlign: 'center' }}>Salam</Text> */}
+      <Text style={{ textAlign: 'center' }}>Salam</Text>
       <View style={styles.imgContainer}>
         <Image resizeMethod={'auto'} resizeMode={"contain"} source={{ width: 600, height: 250, uri: "https://picsum.photos/seed/picsum/200/300" }} style={styles.img} />
       </View>
@@ -17,12 +17,30 @@ const DesignView: React.FC<any> = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  mainView:
-    { width: "100vw", height: "100vh", backgroundColor: "yellow", borderColor: "black", borderWidth: 1, borderStyle: "solid" },
-  imgContainer:
-    { backgroundColor: "rgba(11,222, 123, 0.25)", width: "92%", height: "80%", marginLeft: "4%" },
-  img: { width: "100%", height: "100%" },
-  btnsContainer: { flex: 1, flexDirection: "row", justifyContent: "space-around", backgroundColor: "pink" }
+  mainView: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "yellow",
+    borderColor: "black",
+    borderWidth: 1,
+    borderStyle: "solid"
+  },
+  imgContainer: {
+    backgroundColor: "rgba(11,222, 123, 0.25)",
+    width: "92%",
+    height: "80%",
+    marginLeft: "4%"
+  },
+  img: {
+    width: "100%",
+    height: "100%"
+  },
+  btnsContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "pink"
+  }
 })
 
 export default DesignView;
